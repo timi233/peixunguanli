6dd5039ef422450c21e2d9b81d4153617ebc2cc2
张健 <zhangjian@example.com>
2026-03-13 17:54:08 +0800

    fix: 修复 CertificateTable.tsx 中文冒号

--- components/CertificateTable.tsx
+++ components/CertificateTable.tsx
@@ -3,11 +3,11 @@ import { ExclamationCircleOutlined, CheckCircleOutlined, CloseCircleOutlined } f
 
 interface Certificate {
   record_id: string;
-  关联人员：string;
-  证书：string;
-  获得日期：string;
-  有效期至：string;
-  证书状态：'有效' | '即将到期' | '已过期' | '已冻结';
+  关联人员:string;
+  证书:string;
+  获得日期:string;
+  有效期至:string;
+  证书状态:'有效' | '即将到期' | '已过期' | '已冻结';
 }
 
 interface CertificateTableProps {
